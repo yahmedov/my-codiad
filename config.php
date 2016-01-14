f1cb35f40b8d1c6b42b9ca3d652e07fd647e08eb
yahmedov <ahmedov.yuksel@gmail.com>
2016-01-14 14:32:35 +0200

    edited config.php
    // A Redirect needs to be set in the server  "src.qa.soft-intellect.com//home/qasoft8/public_html" -> "http://qa.soft-intellect.com/" for attached projects

--- config.php
+++ config.php
@@ -11,25 +11,22 @@
 //////////////////////////////////////////////////////////////////
 
 // PATH TO CODIAD
-define("BASE_PATH", "absolute/path/to/codiad");
+define("BASE_PATH", "/home/qasoft8/public_html/codiad");
 
 // BASE URL TO CODIAD (without trailing slash)
-define("BASE_URL", "domain.tld");
+define("BASE_URL", "qa.soft-intellect.com/codiad");
 
 // THEME : default, modern or clear (look at /themes)
 define("THEME", "default");
 
-// ABSOLUTE PATH, this is used as whitelist for absolute path projects 
+// ABSOLUTE PATH
 define("WHITEPATHS", BASE_PATH . ",/home");
 
 // SESSIONS (e.g. 7200)
 $cookie_lifetime = "0";
 
 // TIMEZONE
-date_default_timezone_set("America/Chicago");
-
-// Allows to overwrite the default language
-//define("LANGUAGE", "en");
+date_default_timezone_set("Europe/Sofia");
 
 // External Authentification
 //define("AUTH_PATH", "/path/to/customauth.php");
@@ -46,7 +43,10 @@ define("DATA", BASE_PATH . "/data");
 define("WORKSPACE", BASE_PATH . "/workspace");
 
 // URLS
-define("WSURL", BASE_URL . "/workspace");
+//define("WSURL", BASE_URL . "/workspace"); //original "WSURL" value
+define("WSURL", "src.qa.soft-intellect.com"); // in order to open workspace files in "src.qa.soft-intellect.com" which points to "/codiad/workspace"
+// A Redirect needs to be set in the server  "src.qa.soft-intellect.com//home/qasoft8/public_html" -> "http://qa.soft-intellect.com/" for attached projects
+
 
 // Marketplace
 //define("MARKETURL", "http://market.codiad.com/json");
@@ -55,5 +55,3 @@ define("WSURL", BASE_URL . "/workspace");
 //define("UPDATEURL", "http://update.codiad.com/?v={VER}&o={OS}&p={PHP}&w={WEB}&a={ACT}");
 //define("ARCHIVEURL", "https://github.com/Codiad/Codiad/archive/master.zip");
 //define("COMMITURL", "https://api.github.com/repos/Codiad/Codiad/commits");
-
-?>
